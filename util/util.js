8171ee1c5d913380b8a0967ecae259763043f1a7
hoangbeo69 <nguyenminhhoang2709@gmail.com>
2023-12-07 14:40:55 +0700

    commit code and image screen shot

--- util/util.js
+++ util/util.js
@@ -6,13 +6,6 @@ import { dirname, join, normalize } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// filterImageFromURL
-// helper function to download, filter, and save the filtered image locally
-// returns the absolute path to the local image
-// INPUTS
-//    inputURL: string - a publicly accessible url to an image file
-// RETURNS
-//    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL) {
   return new Promise(async (resolve, reject) => {
     try {
@@ -23,9 +16,9 @@ export async function filterImageFromURL(inputURL) {
         "/tmp/filtered"
       )}-${Math.floor(Math.random() * 2000)}.jpg`;
       await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
+        .resize(256, 256) 
+        .quality(60) 
+        .greyscale() 
         .write(outpath, (img) => {
           resolve(outpath);
         });
@@ -34,12 +27,6 @@ export async function filterImageFromURL(inputURL) {
     }
   });
 }
-
-// deleteLocalFiles
-// helper function to delete files on the local disk
-// useful to cleanup after tasks
-// INPUTS
-//    files: Array<string> an array of absolute paths to files
 export async function deleteLocalFiles(files) {
   for (let file of files) {
     fs.unlinkSync(file);
